3d8f3dba61ddf1592cb5b5f7153d810670c80dfb
shans10 <shantanu.shukla10@gmail.com>
2021-03-14 22:54:06 +0530

    changed double random function in main.h

--- src/main.h
+++ src/main.h
@@ -4,7 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
-#include <cstdlib>
+#include <random>
 
 // Use statements
 using std::shared_ptr;
@@ -21,12 +21,13 @@ inline double degrees_to_radians(double degrees) {
 }
 
 inline double random_double() {
-    // Returns a random real in [0,1]
-    return rand() / (RAND_MAX + 1.0);
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
 }
 
 inline double random_double(double min, double max) {
-    // Returns  a random real in [min,max]
+    // Returns a random real number in [min,max].
     return min + (max-min)*random_double();
 }
 
