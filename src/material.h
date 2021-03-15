f4df17c193d9a8aacba6b7f4f8a873332b293a49
shans10 <shantanu.shukla10@gmail.com>
2021-03-15 10:58:16 +0530

    completed and fixed Metal

--- src/material.h
+++ src/material.h
@@ -42,6 +42,7 @@ class metal : public material {
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const override {
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected);
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
