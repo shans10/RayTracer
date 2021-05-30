9b974bb5f3f3d605672d6b700ae269e5999bbadb
Sharad Patel <sharadp1308@gmail.com>
2021-05-30 14:52:09 +0530

    partially completed BVH

--- src/sphere.h
+++ src/sphere.h
@@ -13,6 +13,8 @@ class sphere : public hittable {
         virtual bool hit(
             const ray& r, double t_min, double t_max, hit_record& rec) const override;
 
+        virtual bool bounding_box(double time0, double time1, aabb& output_box) const override;
+
     public:
         point3 center;
         double radius;
@@ -44,6 +46,14 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
     rec.mat_ptr = mat_ptr;
 
     return true;
+}
+
+bool sphere::bounding_box(double time0, double time1, aabb& output_box) const {
+    output_box = aabb(
+        center - vec3(radius, radius, radius),
+        center + vec3(radius, radius, radius));
+
+        return true;
 }
 
 #endif
