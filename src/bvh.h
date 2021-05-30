4cfd9af3e93771e37fe33138f58bc58705148ce0
Sharad Patel <sharadp1308@gmail.com>
2021-05-30 16:05:48 +0530

    partially completed BVH

--- src/bvh.h
+++ src/bvh.h
@@ -20,7 +20,7 @@ class bvh_node : public hittable {
             size_t start, size_t end, double time0, double time1));
 
         virtual bool hit(
-            const ray& r, double t_min, double t_max, hit_record& record) const override;
+            const ray& r, double t_min, double t_max, hit_record& rec) const override;
         
         virtual bool bounding_box(double time0, double time1, aabb& output_box) const override;
 
@@ -35,7 +35,7 @@ bool bvh_node :: bounding_box(double time0, double time1, aabb& output_box) cons
     return true;
 }
 
-virtual bool hit(const ray& r, double t_min, double t_max, hit_record& record) const{
+bool bvh_node :: hit(const ray& r, double t_min, double t_max, hit_record& rec) const{
     if(!box.hit(r, t_min, t_max)
         return false;
 
@@ -45,13 +45,13 @@ virtual bool hit(const ray& r, double t_min, double t_max, hit_record& record) c
     return hit_left || hit_right;
 }
 
-bool bvh_node :: bvh_node(
+bvh_node :: bvh_node(
     std :: vector<shared_ptr<hittable>>& src_objects,
     size_t start, size_t end, double time0, double time1) {
         auto objects = src_objects;
 
         int axis = random_int(0,2);
-        auto comprator = (axis = 0) ? box_x_compare
+        auto comparator = (axis = 0) ? box_x_compare
                        : (axis = 1) ? box_y_compare
                                     : box_z_compare;
 
@@ -60,7 +60,7 @@ bool bvh_node :: bvh_node(
         if(object_span == 1)
             left = right = objects[start];
         else if(object_span == 2) {
-            if(comprator(objects[start], objects[start + 1])) {
+            if(comparator(objects[start], objects[start + 1])) {
                 left = objects[start];
                 right = objects[start +1];
             }
@@ -70,7 +70,7 @@ bool bvh_node :: bvh_node(
             }
         }
         else {
-            std::sort(objects.begin() + start, objects.begin() + end, comprator);
+            std::sort(objects.begin() + start, objects.begin() + end, comparator);
             auto mid = start + object_span/2;
             left = make_shared<bvh_node>(objects, start, mid, time0, time1);
             right = make_shared<bvh_node>(objects, mid, end, time0, time1);
@@ -81,7 +81,7 @@ bool bvh_node :: bvh_node(
         if ( !left->bounding_box(time0, time1, box_left)
           || !right->bounding_box(time0, time1, box_right)
         )
-            std::cer<<"No bounding_box in bvh_node constructor .\n";
+            std::cerr<<"No bounding_box in bvh_node constructor .\n";
 
         box = surrounding_box(box_left, box_right);
 }
@@ -91,7 +91,7 @@ inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable>
     aabb box_b;
 
     if(!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
-        std::cer<<"No bounding_box in bvh_node constructor .\n";
+        std::cerr<<"No bounding_box in bvh_node constructor .\n";
     
     return box_a.min().e[axis] < box_b.min().e[axis];
 }
