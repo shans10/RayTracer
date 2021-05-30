025af74c81c2180c7c672bd4af6d8ebc1c9e41ce
Shantanu Shukla <shantanu.shukla10@gmail.com>
2021-05-30 15:26:27 +0530

    fixed aabb.h

--- src/aabb.h
+++ src/aabb.h
@@ -2,34 +2,36 @@
 #define AABB_H
 
 #include "main.h"
+#include <math.h>
 
 class aabb {
     public :
         aabb() {}
-        aabb(const point3& a, const point3& b) {minimum = a; maximum = b;}
+        aabb(const point3& a, const point3& b) { minimum = a; maximum = b; }
 
-        point3 min() cons{return minimum;}
-        point3 max() cons{return maximum;}
+        point3 min() const { return minimum; }
+        point3 max() const { return maximum; }
 
-        inline bool aabb :: hit(const ray& r, double t_min, double t_max) const {
-            for(int a=0; a<3, a++) {
-                auto invD = 1.0f / r.direction()[a];
-                auto t0 = (min()[a] - r.origin()[a]) * invD;
-                auto t1 = (max()[a] - r.origin()[a]) * invD;
+        bool hit(const ray& r, double t_min, double t_max) const;
 
-                if(invD < 0.0f)
-                    std :: swap(t0, t1);
-                
-                t_min = t0 > t_min ? t0 : t_min;
-                t_max = t1 < t_max ? t1 : t_max;
-                if(t_max <= t_min)
-                    return false;
-            }
-            return true;
-        }
         point3 minimum;
         point3 maximum;
 };
+
+inline bool aabb::hit(const ray& r, double t_min, double t_max) const {
+    for (int a = 0; a < 3; a++) {
+        auto invD = 1.0f / r.direction()[a];
+        auto t0 = (min()[a] - r.origin()[a]) * invD;
+        auto t1 = (max()[a] - r.origin()[a]) * invD;
+        if (invD < 0.0f)
+            std::swap(t0, t1);
+        t_min = t0 > t_min ? t0 : t_min;
+        t_max = t1 < t_max ? t1 : t_max;
+        if (t_max <= t_min)
+            return false;
+    }
+    return true;
+}
 
 aabb surrounding_box(aabb box0, aabb box1) {
     point3 small(fmin(box0.min().x(), box1.min().x()),
@@ -43,4 +45,4 @@ aabb surrounding_box(aabb box0, aabb box1) {
     return aabb(small, big);
 }
 
-#endif;
+#endif
