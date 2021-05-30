62386b49b36c34d040eb9c8d22fffda832f089e5
Sharad Patel <Dante13@localhost.localdomain>
2021-05-30 14:52:09 +0530

    partially completed BVH

--- src/main.h
+++ src/main.h
@@ -37,6 +37,10 @@ inline double clamp(double x, double min, double max) {
     return x;
 }
 
+inline int random_int(int min, int max) {
+    return static_cast<int>(random_double(min, max + 1));
+}
+
 // Common Headers
 #include "ray.h"
 #include "vec3.h"
