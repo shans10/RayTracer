1cb984ab7fec5f10f482f3486d0418e9e32494d9
Sharad Patel <Dante13@localhost.localdomain>
2021-05-30 16:05:48 +0530

    partially completed BVH

--- src/hittable_list.h
+++ src/hittable_list.h
@@ -51,7 +51,7 @@ bool hittable_list::bounding_box(double time0, double time1, aabb& output_box) c
     bool first_box = true;
 
     for(const auto& object : objects) {
-        if(!objects -> bounding_box(time0, time1, temp_box))
+        if(!object -> bounding_box(time0, time1, temp_box))
             return false;
         output_box = first_box ? temp_box : surrounding_box(output_box, temp_box);
         first_box = false;
