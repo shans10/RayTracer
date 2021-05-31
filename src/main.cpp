3df7d13b12d203fea582f4121fe16a40fb2e88ec
Shantanu Shukla <shantanu.shukla10@gmail.com>
2021-05-31 22:54:32 +0530

    completed Image-Texture-Mapping

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include "moving_sphere.h"
 
 // Multithreading Support
-#include "ThreadPool.h"
+#include "external/ThreadPool.h"
 
 // System Header Files
 #include <iostream>
@@ -37,6 +37,19 @@ color ray_color(const ray& r, const hittable& world, int depth) {
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
+
+// Earth Sphere
+hittable_list earth() {
+    // By default stb_image library will look for the image file in the same directory as the executable,
+    // So either copy the 'earthmap.jpg' in the same directory as the executable i.e. "build/" on Linux or "build/Release" on Windows,
+    // Or give the absolute path to the image file which is found in the base directory of the repository.
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
+
+    return hittable_list(globe);
+}
+
 
 // Two Perlin Spheres
 hittable_list two_perlin_spheres() {
@@ -192,10 +205,17 @@ int main() {
             vfov = 20.0;
             break;
 
-        default:
         case 3:
             world = two_perlin_spheres();
             lookfrom = point3(13, 2 ,3);
+            lookat = point3(0, 0, 0);
+            vfov = 20.0;
+            break;
+
+        default:
+        case 4:
+            world = earth();
+            lookfrom = point3(13, 2, 3);
             lookat = point3(0, 0, 0);
             vfov = 20.0;
             break;
